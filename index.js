ef6ab41e1560b97c3acbeced92cfc0f07be03a94
Lamtin <lamtin@qq.com>
2014-10-21 15:41:08 +0800

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = function(content, file, conf) {
+
+  var fileReg = /<!--#include\ssrc="([^"]+)"\s*-->/gim;
+
+  if(file.isHtmlLike){
+    content = content.replace(fileReg,function(ret, src){
+      src = fis.project.getProjectPath() + '/modules/' + src;
+      if (fis.util.isFile(src)) {
+        var tpl = fis.file.wrap(src);
+        return tpl.getContent();
+      }
+    });
+  }
+
+  return content;
+};
