6fc9b762baa73dd8c70ff7e1ab22c6c2d2cb1367
软件工程师李飛 <lifei@7v1.net>
2015-09-23 15:15:06 +0800

    调整替换的路径
    项目下任何文件都可以被include

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function(content, file, conf) {
 
   if(file.isHtmlLike){
     content = content.replace(fileReg,function(ret, src){
-      src = fis.project.getProjectPath() + '/modules/' + src;
+      src = fis.project.getProjectPath() + '/' + src;
       if (fis.util.isFile(src)) {
         var tpl = fis.file.wrap(src);
         return tpl.getContent();
